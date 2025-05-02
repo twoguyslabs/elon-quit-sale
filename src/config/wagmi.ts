f1f99b0e875ac14bc697c434f70eb4b05ff5007a
Two Guys Labs <2guyslabs@gmail.com>
2025-05-02 21:50:40 +0700

    Refactor wagmi configuration and ReownContextProvider; standardize code formatting and improve readability. Ensure projectId validation is consistent across files.

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,24 +1,24 @@
-import { cookieStorage, createStorage } from '@wagmi/core'
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { bsc, bscTestnet } from '@reown/appkit/networks'
+import { cookieStorage, createStorage } from "@wagmi/core";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import { bsc, bscTestnet } from "@reown/appkit/networks";
 
 // Get projectId from https://cloud.reown.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
 if (!projectId) {
-    throw new Error('Project ID is not defined')
+  throw new Error("Project ID is not defined");
 }
 
-export const networks = [bsc, bscTestnet]
+export const networks = [bsc, bscTestnet];
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
-    storage: createStorage({
-        storage: cookieStorage
-    }),
-    ssr: true,
-    projectId,
-    networks
-})
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  ssr: true,
+  projectId,
+  networks,
+});
 
-export const config = wagmiAdapter.wagmiConfig
+export const config = wagmiAdapter.wagmiConfig;
